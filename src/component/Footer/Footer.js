1a92fd72559ed1386025b28ae99e3ecc24fba96b
isha2510 <ishatripathi80@gmail.com>
2018-01-31 22:26:36 +0530

    Add files via upload

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+
+
+const Footer= () => {
+
+
+    return (
+      <div className="App">
+
+    <p>  My footer </p>
+      
+      </div>
+    );
+  }
+
+
+export default Footer;
